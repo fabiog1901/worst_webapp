233481d298146e31302d4ba561bc91f47b521b7e
Fabio Ghirardello <fabio@cockroachlabs.com>
2023-05-22 12:33:47 -0400

    ..

--- webapp/src/views/uikit/File.vue
+++ webapp/src/views/uikit/File.vue
@@ -13,10 +13,10 @@ const onUpload = () => {
         <div class="col-12">
             <div class="card">
                 <h5>Advanced</h5>
-                <FileUpload name="demo[]" @uploader="onUpload" :multiple="true" accept="image/*" :maxFileSize="1000000" customUpload />
+                <FileUpload name="demo[]" @uploader="onUpload" :multiple="true" accept="image/*" :maxFileSize="10000000" customUpload />
 
                 <h5>Basic</h5>
-                <FileUpload mode="basic" name="demo[]" accept="image/*" :maxFileSize="1000000" @uploader="onUpload" customUpload />
+                <FileUpload mode="basic" name="demo[]" accept="image/*" :maxFileSize="10000000" @uploader="onUpload" customUpload />
             </div>
         </div>
         <Toast />
